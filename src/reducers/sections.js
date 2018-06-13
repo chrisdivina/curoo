54eb608ae261c95158c09dce8cdc5cde4c0e0d2e
c_divina <Christophe.Divina@mindgeek.com>
2018-06-13 13:59:25 -0400

    update components and sections reducers

---
+++ src/reducers/sections.js
@@ -0,0 +1,37 @@
+import uniqid from 'uniqid';
+
+const ADD_SECTION = 'section/add';
+const REMOVE_SECTION = 'section/remove';
+
+export const addSectionToState = (id, area, component) => {
+  return {
+    type: ADD_SECTION,
+    area,
+    component
+  }
+}
+
+export const removeSectionFromState = id => {
+  return {
+    type: REMOVE_SECTION,
+    id
+  }
+}
+
+export default (state = {}, action) {
+  switch (action.type) {
+    case ADD_SECTION:
+      const id = uniqid();
+      return {
+        ...state,
+        [id]: {
+          area: action.area,
+          component: action.component
+        }
+      };
+    case REMOVE_SECTION:
+      const { removedSection, ...rest } = state;
+      return { ...rest };
+    default: return state;
+  }
+}
